78dd1e267ff76beab592040da97763ff3d7c6f0d
Markus <gerhards@gendis.de>
2025-04-05 23:00:06 +0200

    add player gets point mechanic

--- paddle_right.gd
+++ paddle_right.gd
@@ -1,14 +1,19 @@
-extends CharacterBody2D
+extends StaticBody2D
 
 @export var speed := 400.0
 @export var is_player_one := true
 
-func _physics_process(delta):
+func _process(delta):
 	var input_dir := 0
 	if is_player_one:
 		input_dir = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	else:
 		input_dir = Input.get_action_strength("p2_down") - Input.get_action_strength("p2_up")
 
-	velocity = Vector2(0, input_dir * speed)
-	move_and_slide()
+	position.y += input_dir * speed * delta
+	
+	if position.y < 0:
+		position.y = 0
+	
+	if position.y > 650:
+		position.y=650
--- pong.gd
+++ pong.gd
@@ -4,6 +4,8 @@ extends Node2D
 @onready var paddle_left := $PaddleLeft
 @onready var paddle_right := $PaddleRight
 
+var p1_score = 0
+var p2_score = 0	
 
 func _process(_delta):
 	if ball.position.x < 0 or ball.position.x > get_viewport_rect().size.x:
@@ -12,3 +14,17 @@ func _process(_delta):
 func reset_ball():
 	ball.position = get_viewport_rect().size / 2
 	ball.direction = Vector2(-1 if randf() > 0.5 else 1, randf_range(-0.5, 0.5)).normalized()
+
+
+func _on_wall_left_body_entered(body: Node2D) -> void:
+	if body.name == "ball":
+		p2_score += 1
+		print("Player 2 scores! Score:", p2_score)
+		reset_ball()
+
+
+func _on_wall_right_body_entered(body: Node2D) -> void:
+	if body.name == "ball":
+		p1_score += 1
+		print("Player 1 scores! Score:", p1_score)
+		reset_ball()
